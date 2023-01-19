7a6e8688afb5d43ef24996621f7774ba5372f025
sid9568 <sid20319@gmail.com>
2023-01-19 16:14:28 +0530

    first commit

---
+++ app/javascript/application.js
@@ -0,0 +1,21 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+ 
+import * as bootstrap from "bootstrap"
+ 
+import "./modules/sidebar"
+import "./modules/theme"
+import "./modules/feather"
+ 
+// Charts
+import "./modules/chartjs"
+ 
+// Forms
+import "./modules/flatpickr"
+ 
+// Maps
+import "./modules/vector-maps"
